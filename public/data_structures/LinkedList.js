951c6a2f272e03c28cb2e989138b2131683ad287
astelvida <sevda.m.anefi@gmail.com>
2017-03-27 01:59:15 -0700

    added removeHead, contains and deleteNode to LinkedList class

--- public/data_structures/LinkedList.js
+++ public/data_structures/LinkedList.js
@@ -1,4 +1,5 @@
-export class List {
+
+class List {
 
   constructor() {
     this.head = null;
@@ -14,16 +15,48 @@ export class List {
     if (this.tail) {
       this.tail.next = node;
     }
-
     this.tail = node;
   }
 
   removeHead() {
-
+    if (list.head === null) {
+      return null;
+    }
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    return currentHead;
   }
 
-  deleteNode() {
+  contains(target) {
+    this.head = node;
+    while(node !== null) {
+      if(node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
+  }
 
+  deleteNode(target) {
+    let node = this.head;
+
+    if(node && node.value === target) {
+      return this.removeHead();
+    }
+
+    while(node.next !== null) {
+      if(node.next.value === target) {
+        var deleted = node.next;
+        node.next = node.next.next;
+        // update tail if deeleting the last node;
+        if(deleted === this.tail) {
+          this.tail = node;
+        }
+        return deleted;
+      }
+      node = node.next;
+    }
   }
 
 }
@@ -42,5 +75,7 @@ list.appendToTail(1);
 list.appendToTail(5);
 list.appendToTail(13);
 list.appendToTail(7);
+
+list.deleteNode(7);
 
 console.log(list)
