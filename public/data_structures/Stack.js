ba300ec8a0c7e2ebf0f30cdc68b0ff7975e9354c
astelvida <sevda.m.anefi@gmail.com>
2017-03-27 20:46:16 -0700

    implemented stack data structure

--- public/data_structures/Stack.js
+++ public/data_structures/Stack.js
@@ -1,3 +1,34 @@
 function Stack() {
-  // let obj
+  const obj = {};
+  const storage = {};
+  let size = 0;
+
+  obj.push = (item) => {
+    storage[size++] = item;
+  };
+
+  obj.pop = () => {
+    /*
+      expression on the right will only execute only if the left evalutes
+      to true. So if size === 0, i.e. false size will not be decreased and
+      result will be undefined;
+    */
+    size && size--;
+    const popped = storage[size];
+    delete storage[size];
+    return popped;
+  };
+
+  obj.size = () => size;
+
+  obj.isEmpty = () => !obj.size();
+
+  return obj;
 }
+
+const stack = Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.pop();
