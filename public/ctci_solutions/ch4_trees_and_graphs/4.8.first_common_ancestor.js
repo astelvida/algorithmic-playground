2007b64e59c4a0250184cf2cb73c1a846d60ccba
astelvida <sevda.m.anefi@gmail.com>
2017-04-01 19:17:56 -0700

    first common ancestor solution when parent is not known

--- public/ctci_solutions/ch4_trees_and_graphs/4.8.first_common_ancestor.js
+++ public/ctci_solutions/ch4_trees_and_graphs/4.8.first_common_ancestor.js
@@ -1,15 +1,16 @@
-function FCA(tree, n1, n2) {
-  // finde depths;
+// WITH links to parents
+function FCA1(tree, n1, n2) {
+  // if any node is not in the tree, return null
+  if (!n1 || !n2) {
+    return null;
+  }
+  // find depth
   function depth(node, d) {
-    if (node === null) {
-      return d - 1;
-    }
-    return depth(node.parent, d + 1);
+    return node === null? d - 1: depth(node.parent, d + 1);
   }
   const delta = depth(n1, null) - depth(n2, null);
   let first = delta > 0? n1: n2;
-  let second = delta < 0? n1: n2;
-
+  let second = delta <= 0? n1: n2;
   let diff = Math.abs(delta);
 
   // level out
@@ -25,8 +26,49 @@ function FCA(tree, n1, n2) {
   }
 
   return first;
-  // return delta;
 }
+
+// WITHOUT links to parents
+function FCA2(root, n1, n2) {
+  // if any node is not in the tree, return null
+  if (!cover(root, n1) || !cover(root, n2)) {
+    return null;
+  }
+  console.log('n1 is:', n1.val, 'n2 is:', n2.val);
+  function intersection(root, n1, n2) {
+
+    // base case is when one of the nodes is the root
+    if (root === null || root === n1 || root === n2) {
+      return root.val;
+    }
+
+    // checks if n1 and n2 are on the left side
+    const n1IsOnLeft = cover(root.left, n1);
+    const n2IsOnLeft = cover(root.left, n2);
+
+    console.log(`for root ${root.val}:
+    n1IsOnLeft=${n1IsOnLeft} and n2IsOnLeft=${n2IsOnLeft}`);
+
+    // if nodes are on different sides, return the current root;
+    // when one of the boolean flags is false means one of them is root
+    if (n1IsOnLeft !== n2IsOnLeft) {
+      return root;
+    }
+    // if they are on the same side, keep looking one level down
+    const levelDown = n1IsOnLeft? root.left : root.right;
+    return intersection(levelDown, n1, n2);
+  }
+
+  function cover(root, target) {
+    // console.log('root:', root? root.val: null)
+    if (root === null) return false;
+    if (target === root) return true;
+    return cover(root.left, target) || cover(root.right, target);
+  }
+
+  return intersection(root, n1, n2);
+}
+
 
 
 function Node(val=null, parent=null) {
@@ -34,7 +76,7 @@ function Node(val=null, parent=null) {
   node.val = val;
   node.right = null;
   node.left = null;
-  node.parent = parent;
+  // node.parent = parent;
   return node;
 }
 
@@ -51,8 +93,8 @@ tree.right.right = Node('f', tree.right);
 tree.right.right.right = Node('h', tree.right.right);
 tree.right.right.left = Node('g', tree.right.right);
 
-const n1 = tree.left.left.right; // t
-const n2 = tree.right; // b
+const n1 = tree.left.right; // e
+const n2 = tree.left.left.right; // t
   // console.log('nodes:', JSON.stringify(tree, null, '\t'));
 
-  console.log('should return "a"', FCA(tree, n1, n2));
+  console.log('should return "a"', FCA2(tree, n1, n2));
