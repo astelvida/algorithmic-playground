224f41d6dcf363db86e355b1d543afa3a1a15b8d
astelvida <sevda.m.anefi@gmail.com>
2017-04-01 18:34:23 -0700

    solution 2 gto fitst common ancestor

--- public/ctci_solutions/ch4_trees_and_graphs/4.8.first_common_ancestor.js
+++ public/ctci_solutions/ch4_trees_and_graphs/4.8.first_common_ancestor.js
@@ -1,66 +1,32 @@
 function FCA(tree, n1, n2) {
-  let d1 = null;
-  let d2 = null;
-
   // finde depths;
-  function dfs(node, depth, parent) {
-    console.log('node', node.val, 'depth', depth, 'parent', parent? parent.val : null)
-
+  function depth(node, d) {
     if (node === null) {
-      return;
+      return d - 1;
     }
-    node.visited = true;
-    // found both depths and we can safely return;
-    if (d1 && d2) {
-      return true;
-    } else if (node.val === n1.val) {
-      d1 = depth;
-    } else if (node.val === n2.val) {
-      d2 = depth;
-    }
-
-    if (node.left && !node.left.visited) {
-      dfs(node.left, depth + 1, node);
-    }
-    if (node.right && !node.right.visited) {
-      dfs(node.right, depth + 1, node);
-    }
+    return depth(node.parent, d + 1);
   }
-  dfs(tree, 0, null);
-  console.log('d1:', d1, 'd2:', d2 )
-  let max;
-  let min;
-  let node1;
-  let node2;
-  if (d1 > d2) {
-    max = d1;
-    min = d2;
-    node1 = n1;
-    node2 = n2;
-  } else {
-    max = d2;
-    min = d1;
-    node1 = n2;
-    node2 = n1;
+  const delta = depth(n1, null) - depth(n2, null);
+  let first = delta > 0? n1: n2;
+  let second = delta < 0? n1: n2;
+
+  let diff = Math.abs(delta);
+
+  // level out
+  while (diff !== 0) {
+    first = first.parent;
+    diff--;
   }
 
-
-  // get them both to save level
-  while (max !== min) {
-    node1 = node1.parent;
-    max--;
-  }
-  if (node1.val === node2.val) return node1;
-  // find their parent at the intersection
-  while (node1 !== node2 ) {
-    node1 = node1.parent;
-    node2 = node2.parent;
+  // loop till we find the intersection
+  while (first !== second) {
+    first = first.parent;
+    second = second.parent;
   }
 
-  return node1.val;
-  // if (d1 !== d2)
+  return first;
+  // return delta;
 }
-
 
 
 function Node(val=null, parent=null) {
@@ -86,7 +52,7 @@ tree.right.right.right = Node('h', tree.right.right);
 tree.right.right.left = Node('g', tree.right.right);
 
 const n1 = tree.left.left.right; // t
-const n2 = tree.left; // b
+const n2 = tree.right; // b
   // console.log('nodes:', JSON.stringify(tree, null, '\t'));
 
   console.log('should return "a"', FCA(tree, n1, n2));
